2dfbe0534cb5575484832cdcb6326d337e1ffc0c
Yaron Parasol <yaronpa@gigaspaces.com>
2012-01-30 12:20:54 +0200

    tree speed

--- scripts/cloudify_default.js
+++ scripts/cloudify_default.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 	//Header buttons
 	
 	//TOC Tree
-	$(".TableOfContents").jstree( {animation: 50, "themes" : {"theme" : "classic", "dots" : false, "icons" : false },
+	$(".TableOfContents").jstree( {core:{animation: 50}, "themes" : {"theme" : "classic", "dots" : false, "icons" : false },
 									plugins:["themes","html_data"]} ).loaded ($(".TableOfContents").css("background-color","transparent")) }
 									
 );
