efe6d8d66bb61cfe4ffab59fb34a5c7bea448446
Yaron Parasol <yaronpa@gigaspaces.com>
2012-01-30 11:38:59 +0200

    animation is faster now

--- scripts/cloudify_default.js
+++ scripts/cloudify_default.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 	//Header buttons
 	
 	//TOC Tree
-	$(".TableOfContents").jstree( {"themes" : {"theme" : "classic", "dots" : false, "icons" : false },
+	$(".TableOfContents").jstree( {animation: 200, "themes" : {"theme" : "classic", "dots" : false, "icons" : false },
 									plugins:["themes","html_data"]} ).loaded ($(".TableOfContents").css("background-color","transparent")) }
 									
 );
