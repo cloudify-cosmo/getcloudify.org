b9815f9258579c5d506aea4ce6f7ba226a536574
Uri Cohen <uri1803@gmail.com>
2012-02-01 00:14:56 +0200

    Testing search

--- scripts/search.js
+++ scripts/search.js
@@ -97,7 +97,7 @@ $(document).ready(function () {
 //                $("#resultDiv").hide();
 //                $("#pageContent").fadeIn();
 //            });
-                    $("#pageContent").hide('fast', function () {
+                    $("#pageContent").hide(function () {
                         $(".pageTitle").text("Search Results");
                         $("#resultDiv").fadeIn();
                         $(".breadcrumb").html('<a href="#">Back to ' + settings.pageTitle + '</a>');
