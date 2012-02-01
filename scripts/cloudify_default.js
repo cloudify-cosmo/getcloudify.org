1b7c2c3ed698a60a92d1f59eae704239cbd41aa0
Yaron Parasol <yaronpa@gigaspaces.com>
2012-02-01 11:28:32 +0200

    replaced tree with accordion

--- scripts/cloudify_default.js
+++ scripts/cloudify_default.js
@@ -1,8 +1,3 @@
-$(document).ready(function () {	
-	//Header buttons
-	
-	//TOC Tree
-	$(".TableOfContents").jstree( {core:{animation: 50}, "themes" : {"theme" : "classic", "dots" : false, "icons" : false },
-									plugins:["themes","html_data"]} ).loaded ($(".TableOfContents").css("background-color","transparent")) }
-									
-);
+$(function() {
+		$( "#accordion" ).accordion();
+});
