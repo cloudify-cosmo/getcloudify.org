da16854de66e0b37aa0b6f823f1e75813accb3d3
Uri Cohen <uri1803@gmail.com>
2012-02-04 21:21:42 +0200

    search fixes

--- scripts/search.js
+++ scripts/search.js
@@ -97,7 +97,7 @@ $(document).ready(function () {
 //                $("#resultDiv").hide();
 //                $("#pageContent").fadeIn();
 //            });
-                    $("#pageContent").hide(1, function () {
+                    $(".DocumentsWrapRight").hide(1, function () {
                         $(".pageTitle").text("Search Results");
                         $("#resultDiv").fadeIn();
                         $(".breadcrumb").html('<a href="#">Back to ' + settings.pageTitle + '</a>');
