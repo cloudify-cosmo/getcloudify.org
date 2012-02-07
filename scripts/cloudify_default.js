14a5ad9c5723ed8b87e8d846a0c5a748ac13f8ea
yaronp68 <yaronp68@gmail.com>
2012-02-07 18:44:34 +0200

    Update scripts/cloudify_default.js

--- scripts/cloudify_default.js
+++ scripts/cloudify_default.js
@@ -1,3 +1,11 @@
 $(function() {
-		$( "#accordion" ).accordion({autoHeight:false, navigation: true,collapsible: true});
+		$( "#accordion" ).accordion({autoHeight:false, navigation: true,collapsible: true,
+		create: function(event, ui) {
+			$(this).children("h3").each(function(i){
+			 if (this.val() == $(".pageTitle").val() ) {
+			 	activate(i);
+			 }
+			})
+		}
+		});
 });
