6e91fad2a75c2e676c29dc1f06217026598ac5f8
Uri Cohen <uri1803@gmail.com>
2012-02-07 19:01:28 +0200

    Update scripts/cloudify_default.js

--- scripts/cloudify_default.js
+++ scripts/cloudify_default.js
@@ -1,5 +1,5 @@
 $(function() {
-		$( "#accordion" ).accordion({autoHeight:false, navigation: true,collapsible: true,
+		$( "#accordion" ).accordion({autoHeight:false, navigation: false,collapsible: true,
 		create: function(event, ui) {
 			$(this).children("h3").each(function(i){
 			 if (this.val() == $(".pageTitle").val() ) {
