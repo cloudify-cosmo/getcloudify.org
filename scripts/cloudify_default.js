5e34fa1c048378c8bf55208817dabcc3e49034a5
yonglas <yonatan@4hilton.com>
2012-02-14 12:11:22 +0200

    Update scripts/cloudify_default.js

--- scripts/cloudify_default.js
+++ scripts/cloudify_default.js
@@ -6,9 +6,9 @@ $(function() {
 	var slides = $('.slide');
 	var numberOfSlides = slides.length;
   
-if ($("#slideshow").exists()) {    // Do something
+if ($("#slideshow").exists()) {    // Do something}
 
-}
+
 	// Remove scrollbar in JS .
   $('#slidesContainer').css('overflow', 'hidden');
 
@@ -44,7 +44,7 @@ if ($("#slideshow").exists()) {    // Do something
       // Move slideInner using margin-left
       $('#slideInner').animate({
         'marginLeft' : slideWidth*(-currentPosition)
-      });
+      //});
   })  
 };
 
