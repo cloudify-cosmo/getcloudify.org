d670a9ec1d685af734b69e6fd194e947049b17b7
yonglas <yonatan@4hilton.com>
2012-03-20 13:38:21 +0200

    Update scripts/cloudify_default.js

--- scripts/cloudify_default.js
+++ scripts/cloudify_default.js
@@ -216,7 +216,20 @@ $.fn.slideFadeUp = function(speed, callback) {
 
 
 
+       $(function() {
 
+	   $(this).removeClass("current");
+
+            $("div.ui-accordion").find("a").each(function(){
+            if( location.href.indexOf(this.location.href) != -1) {
+            $(this).addClass("current");
+             
+            }
+
+            });
+  
+            
+        });
 
 
 
@@ -294,17 +307,3 @@ if ($("#slideshow").exist()) {    // Do something}
 
 
 
-       $(function() {
-
-	   $(this).removeClass("current");
-
-            $("div.ui-accordion").find("a").each(function(){
-            if( location.href.indexOf(this.location.href) != -1) {
-            $(this).addClass("current");
-             
-            }
-
-            });
-  
-            
-        });
