9ea89a1ae629b8553051c18864c8baaf628c5db7
yonglas <yonatan@4hilton.com>
2012-03-22 08:10:44 +0200

    Update scripts/cloudify_default.js

--- scripts/cloudify_default.js
+++ scripts/cloudify_default.js
@@ -236,7 +236,7 @@ $("a.newwindow").attr("target","_blank");
 	$(this).removeClass("current");
 	$("div.ui-accordion").find("a").each(function(){
 		//if( location.href.indexOf(this.location.href) != -1) {
-		if( location.href.indexOf(this.location) != -1) {
+		if( location.href.indexOf(this.location) == -1) {
 		$(this).addClass("current");
 		}
 	
