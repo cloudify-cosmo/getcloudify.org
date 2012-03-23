baba70dbad87e43bc250e029779cfce4ca4a0d94
yonglas <yonatan@4hilton.com>
2012-03-23 07:23:47 +0200

    Update scripts/jquery.prettyPhoto.js

--- scripts/jquery.prettyPhoto.js
+++ scripts/jquery.prettyPhoto.js
@@ -1,8 +1,3 @@
-//FILE QUARANTINED
-
-//Microsoft Online Services has removed jquery.prettyPhoto.js since it was found to match the FILE FILTER= MSO File Filter List 1: <in>*.js file filter.
-
-
 
 /* ------------------------------------------------------------------------
   Class: prettyPhoto
