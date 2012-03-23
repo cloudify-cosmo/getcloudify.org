efa49e05ec4ca622ac022df280f24b52bac93657
Tamir Korem <tamir@gigaspaces.com>
2012-03-23 23:26:02 +0200

    Tamir added generated posts scripts (linux and Windows)

---
+++ utils/copyGeneratedPosts.bat
@@ -0,0 +1,91 @@
+@echo off
+
+@rem This utility copies files and folders from the _site generated folder to the site tree.
+@rem The following files will be copied:
+@rem archives atom.xml blog
+@rem The following folders will be copied:
+@rem tags page* 2011 2012 etc... (all the posts paging related folders.
+
+
+set EXCLUDED_DIRS="css downloads guide images scripts webfonts"
+
+set INCLUDED_FILES=archives,atom.xml,blog
+
+set SRC=REPLACE_WITH_DOCS_ROOT_FOLDER\_site
+set DEST_FOLDER=REPLACE_WITH_DOCS_ROOT_FOLDER
+
+del /s/q %DEST_FOLDER%\tags
+echo ---------------------
+
+del /s/q %DEST_FOLDER%\blog
+echo ---------------------
+  
+@rem Deleting old pages folders content
+set /a pagecounter=1
+set PAGEROOT=%DEST_FOLDER%\page
+:loopPages
+set /a pagecounter=pagecounter+1
+SET currPage=%PAGEROOT%%pagecounter%
+if not exist %currPage% goto :loopYears
+echo Deleting old %currPage% folder files ...
+del /s/q %currPage%\*
+echo ---------------------
+goto :loopPages
+
+@rem Deleting old years(and indirectly months) folders content
+:loopYears
+set /A startYear=2010
+set /A yearsCounter=yearsCounter+1
+SET /A currYear=%startYear%+%yearsCounter%
+set currYearFolder=%DEST_FOLDER%\%currYear%
+if not exist %currYearFolder% goto :looFolders
+echo Deleting old %currYearFolder% folder files ...
+del /s/q %currYearFolder%\*
+echo ---------------------
+goto :loopYears
+
+
+:looFolders
+
+@rem Go over folders and copy only the relevant files and folders
+for /F %%i in ('dir /AD /b %SRC%') do call :copyDir %SRC% %%i
+
+echo Copying archives ...
+copy %SRC%\archives_raw %DEST_FOLDER%\archives
+echo Copying atom.xml ...
+copy %SRC%\atom_raw.xml %DEST_FOLDER%\atom.xml
+echo Copying blog ...
+if not exist %DEST_FOLDER%\blog mkdir %DEST_FOLDER%\blog
+copy %SRC%\blog_raw %DEST_FOLDER%\blog\index.html
+
+echo ---------------------
+
+@rem rename %DEST_FOLDER%\_posts %DEST_FOLDER%\posts
+goto b4End
+
+:copyDir
+set currFolder=%2
+set includeFolder=true
+echo %EXCLUDED_DIRS% | findstr /c:%currFolder% >nul && set includeFolder=false||set includeFolder=true
+if "%includeFolder%" == "true" (
+  echo Copying %currFolder% folder from %1\%2 to %DEST_FOLDER%\%2\...
+  xcopy /S/Y %1\%2 %DEST_FOLDER%\%2\
+  echo Copied %currFolder% folder
+  echo ---------------------
+) else (
+    @rem echo Ingoring %currFolder% folder
+)
+
+  
+goto end
+
+
+:b4End
+rename %DEST_FOLDER%\_posts posts
+pause
+
+
+:end
+
+
+@rem rename something.txt someone.txt
---
+++ utils/copyGeneratedPosts.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# This utility copies files and folders from the _site generated folder to the site tree.
+# The following files will be copied:
+# archives atom.xml blog
+# The following folders will be copied:
+# tags page* 2011 2012 etc... (all the posts paging related folders.
+
+clear
+
+EXCLUDED_DIRS="css downloads guide images scripts webfonts"
+
+#INCLUDED_FILES=archives,atom.xml,blog
+
+SRC=REPLACE_WITH_DOCS_ROOT_FOLDER/_site
+DEST_FOLDER=REPLACE_WITH_DOCS_ROOT_FOLDER
+
+# Deleting old files and folders
+rm -rf ${DEST_FOLDER}/page*
+rm -rf ${DEST_FOLDER}/tags
+rm -rf ${DEST_FOLDER}/blog
+rm -rf ${DEST_FOLDER}/20*
+
+# rename ${DEST_FOLDER}/posts ${DEST_FOLDER}/_posts
+
+aZero=0
+#rem Go over folders and copy ONLY the relevant files and folders
+for currFolder in `ls -l ${SRC} | grep "^d" | awk {'print $9'}`
+ do
+   folderExcluded=`echo ${EXCLUDED_DIRS} | grep -c "${currFolder}"`
+   if [ $folderExcluded -eq $aZero ]; then
+     # This folder is NOT excluded , so it will be copied
+     echo "Copying ${SRC}/${currFolder} ${DEST_FOLDER}/${currFolder} ..."
+     cp -rvf ${SRC}/${currFolder} ${DEST_FOLDER}/${currFolder}
+     echo "Copied ${currFolder} folder"
+     echo "---------------------"
+   fi
+done
+
+echo "Copying archives ..."
+cp ${SRC}/archives_raw ${DEST_FOLDER}/archives
+
+echo "Copying atom.xml ..."
+cp ${SRC}/atom_raw.xml ${DEST_FOLDER}/atom.xml
+
+echo "Copying blog ..."
+newBlogFolder=${DEST_FOLDER}/blog
+if [ ! -d ${newBlogFolder} ]; then
+  mkdir -p ${newBlogFolder}
+fi
+
+cp ${SRC}/blog_raw ${newBlogFolder}/index.html
+
+echo ---------------------
+
+
+mv ${DEST_FOLDER}/_posts ${DEST_FOLDER}/posts
+
+
+
+
