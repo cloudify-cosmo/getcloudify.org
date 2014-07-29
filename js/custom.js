016f9a4d7468508e93e3fac90ab918cc2a094fbb
689df803f755dec0e382660b5ce4ccd20d11dab7 <uri1803@gmail.com>
2014-07-30 02:57:27 +0300

    Improve docs toc responsiveness

--- js/custom.js
+++ js/custom.js
@@ -35,6 +35,65 @@ jQuery(document).ready(function($) {
 		
 	})();
 
+
+			//for highlight current pages
+		//http://docs.shopify.com/support/configuration/store-customization/how-do-i-style-active-link-with-javascript-and-css
+		$('.nav li.topmenu').each(function() {
+			var href = $(this).find('a').attr('href');
+			if (href === window.location.pathname) {
+				$(this).addClass('active');
+				if($(this).parent().parent().hasClass("topmenu")){
+					$(this).parent().parent().addClass('active');
+				}
+			}
+		});
+		
+		//highlight  docs menu links
+		$('a.list-group-item').each(function() {
+			if ($(this).attr('href')  ===  window.location.pathname) {
+				$(this).addClass('highlight');
+				$(this).parent().show();
+				$('.icon-caret-down').attr('class','icon-caret-right');
+				$(this).parent().prev().addClass("active");
+				$(this).parent().prev().children().attr('class','icon-caret-down')
+				var pathArray = window.location.pathname.split( '/' );
+				pathArray = pathArray[2];
+				
+				//highlight top menu when user click on docs menu links
+				$('.nav li.topmenu').each(function() {
+					var href = $(this).find('a').attr('href');
+					var splitHref = href.split( '/' );
+					splitHref=splitHref[2];
+					if (splitHref === pathArray) {
+						$(this).addClass('active');
+						$(this).parent().parent().addClass('active');
+					}
+				});	
+			}
+		});
+		$("#tocAside").css("display", "block");
+
+		//Left docs menu control - open ,close and highlight.
+		$( ".titleHolder").click(function( event ) { 
+		  event.preventDefault();
+		  if($(this).hasClass('active')){
+					if($(this).next().is(':visible')){
+						 $('.icon-caret-down').attr('class','icon-caret-right');
+					}else{
+						$(this).children().attr('class','icon-caret-down');
+					}
+					$(this).next().toggle("fast");
+		  }else{
+			  $('.icon-caret-down').attr('class','icon-caret-right');
+			  $(".titleHolder.active").next().hide("fast");
+			  $(".titleHolder").removeClass('active');
+			  $(this).addClass("active");
+			  $(this).children().attr('class','icon-caret-down');
+			  $(this).next().show("fast");
+		  }
+		});
+
+
 	
 		  $('.toggle-link').each(function() {
 			$(this).click(function() {
@@ -329,67 +388,3 @@ function onPlayerStateChange(event) {
 
 //////////////////////////End YouTube Player  Data //////////////////////////////
 
-
-
-$(window).load(function() {
-
-		//for highlight current pages
-		//http://docs.shopify.com/support/configuration/store-customization/how-do-i-style-active-link-with-javascript-and-css
-		$('.nav li.topmenu').each(function() {
-			var href = $(this).find('a').attr('href');
-			if (href === window.location.pathname) {
-				$(this).addClass('active');
-				if($(this).parent().parent().hasClass("topmenu")){
-					$(this).parent().parent().addClass('active');
-				}
-			}
-		});
-		
-		//highlight  docs menu links
-		$('a.list-group-item').each(function() {
-			if ($(this).attr('href')  ===  window.location.pathname) {
-				$(this).addClass('highlight');
-				$(this).parent().show( "slow" );
-				$('.icon-caret-down').attr('class','icon-caret-right');
-				$(this).parent().prev().addClass("active");
-				$(this).parent().prev().children().attr('class','icon-caret-down')
-				var pathArray = window.location.pathname.split( '/' );
-				pathArray = pathArray[2];
-				
-				//highlight top menu when user click on docs menu links
-				$('.nav li.topmenu').each(function() {
-					var href = $(this).find('a').attr('href');
-					var splitHref = href.split( '/' );
-					splitHref=splitHref[2];
-					if (splitHref === pathArray) {
-						$(this).addClass('active');
-						$(this).parent().parent().addClass('active');
-					}
-				});
-				
-				
-			}
-		});
-
-		//Left docs menu control - open ,close and highlight.
-		$( ".titleHolder").click(function( event ) { 
-		  event.preventDefault();
-		  if($(this).hasClass('active')){
-					if($(this).next().is(':visible')){
-						 $('.icon-caret-down').attr('class','icon-caret-right');
-					}else{
-						$(this).children().attr('class','icon-caret-down');
-					}
-					$(this).next().toggle("slow");
-		  }else{
-			  $('.icon-caret-down').attr('class','icon-caret-right');
-			  $(".titleHolder.active").next().hide("slow");
-			  $(".titleHolder").removeClass('active');
-			  $(this).addClass("active");
-			  $(this).children().attr('class','icon-caret-down');
-			  $(this).next().show( "slow" );
-		  }
-		});
-
-
-});
