6794e18cc4c3ee15ea54241be165edb80edea9f5
689df803f755dec0e382660b5ce4ccd20d11dab7 <uri1803@gmail.com>
2014-08-04 16:36:23 +0300

    Adjust search autocomplete to include categories and better highlighting

--- js/search/search.js
+++ js/search/search.js
@@ -1,175 +1,94 @@
-$(document).ready(function () {
-    var cxBlog = "005646302152591029507:ndo3tbbopl0";
-    var guideCxs = {
-        "2.2":"005646302152591029507:mb6a9za1aoa", 
-        "2.3":"005646302152591029507:zenlau0cpto", 
-        "2.5":"005646302152591029507:akzqpmzyauq", 
-        "2.6":"005646302152591029507:gsfwsaitfsa", 
-        "2.7":"005646302152591029507:eywmbetvnfu"
-    }    
-    var cxEntireSite = "005646302152591029507:wp1h0fve318";
+//$(document).ready(function () {
 
-    $("resultsDiv").hide();
-    var pageTitle = $(".pageTitle").text();
-    var config = {
-        apiURL:'https://www.googleapis.com/customsearch/v1',
-        apiKey:'AIzaSyCR79snpFgr45ear_SBoqkjQaGa7FHYg4I', // Change this to your site
-        cx: function() {
-            href = window.location.href; 
-            var re = /.*\/guide\/(\d+\.\d+)\/.*/;
-            var regexArray = re.exec(href);
-            if (regexArray && regexArray.length > 1) {
-                return guideCxs[regexArray[1]];    
+    var autocompleteRenderFunction = function(document_type, item) {
+        var title = item["highlight"]["title"];
+        var section = getSection(item["url"]);
+    
+        if (!title) {
+            title = item["title"]
+        }
+        var sectionText = section.section != "gen"? "[" + section.desc + "] " : ""; 
+        var out = "<p class='title'>" + sectionText + title + "</p>";
+        if (item["highlight"]["sections"]) {
+            return out.concat("<p class='sections'>" + item["highlight"]["sections"] + "</p>");
+        }
+        return out;
+    }
+
+    var autocompleteResultRenderFunction = function(ctx, results) {
+        var $list = ctx.list, config = ctx.config;
+        
+        var currentSection = getSection(window.location.href);
+
+        $.each(results, function(document_type, items) {
+          $.each(items, function(idx, item) {
+            if (shouldIncludeInResults(currentSection, getSection(item["url"]))) {
+                ctx.registerResult($('<li>' + config.renderFunction(document_type, item) + '</li>').appendTo($list), item);
             }
-            if (href.match(".*/blog.*") || href.match(".*/20.*") || href.match(".*/page.*") || href.match(".*/tags/.*")) 
-                return cxBlog;
-            return cxEntireSite;
-        }(),
-        perPage:10, // A maximum of 10 is allowed by Google
-        page:0, // The start page
-        pageTitle:pageTitle
+          });
+        });
     }
 
-   // $('#searchForm').submit(function () {
-       // googleSearch();
-       // return false;
-   // });
+    var resultRenderFunction = function(document_type, item) {
+        
+        var section = getSection(item["url"]);    
+        
+        var sectionText = section.section != "gen"? "[" + section.desc + "] " : ""; 
+
+        var out = '<a href="' + item['url'] + '" class="st-search-result-link">' + sectionText + item['title'] + '</a>';
+        return out.concat('<p class="genre">' + item['genre'] + '</p>');
+    }
+
+    var Swiftype = window.Swiftype || {};
+    (function() {
+        Swiftype.key = 'MdeBSe5BbjKxTiXEYcxH';
+
+        Swiftype.searchFunctionalBoosts = {
+            "page": {"score": "exponential"}
+        };
+
+        Swiftype.autocompleteRenderFunction = autocompleteRenderFunction; 
+        Swiftype.autocompleteResultRenderFunction = autocompleteResultRenderFunction;
+        //Swiftype.renderFunction = autocompleteRenderFunction;
+
+        /** DO NOT EDIT BELOW THIS LINE **/
+        var script = document.createElement('script'); script.type = 'text/javascript'; script.async = true;
+        script.src = "//s.swiftypecdn.com/embed.js";
+        var entry = document.getElementsByTagName('script')[0];
+        entry.parentNode.insertBefore(script, entry);
+    }());
+
+    function shouldIncludeInResults(currentSection, section) {
+        if (section.section != "guide") return true; 
+        if (currentSection.section != "guide") {
+            return section.section != "guide" || section.version == "3.0";
+        }
+        return currentSection.version == section.version;         
+    }
+
+    function endsWith(str, suffix) {
+        return str.indexOf(suffix, str.length - suffix.length) !== -1;
+    }
+
+    function startsWith(str, prefix) {
+        return str.indexOf(prefix) == 0;
+    }
+
+    function getSection(url) {
+        if (url) {
+            var urlSections = url.split("/");
+            if (urlSections.length > 3) {
+                if (urlSections[3] == "guide") {
+                    var version = urlSections.length > 4? urlSections[4]:null; 
+                    return {desc: version + " Docs", version:version, section:"guide"};
+                } else if (urlSections[3] == "blog" || urlSections[3] == "tags" || $.isNumeric(urlSections[3])) {
+                    return {desc: "Blog", section:"blog"};
+                } 
+            }             
+        }
+        return {desc: "", section:"gen"};            
+    }
     
-    
-   $('#goBtn').click(function () {
-        googleSearch();
-        return false;
-    });
 
 
-
-$('#search').keydown(function(e){
- //enter button in ASCII code
- if(e.keyCode == 13){
-  $("#goBtn").click();
-  return false;
- }
-});
-
-
-
-
-    function successHandler(r) {
-
-    }
-
-    function googleSearch(settings) {
-
-        // If no parameters are supplied to the function,
-        // it takes its defaults from the config object above:
-        settings = $.extend({}, config, settings);
-        settings.term = $('#search').val();
-
-        var resultsDiv = $('#resultsDiv');
-
-        try {
-            $.ajax({
-                type:'GET',
-                url:settings.apiURL,
-                dataType:'jsonp',
-                data:{q:settings.term,
-                    key:settings.apiKey,
-                    num:settings.perPage,
-                    start:((settings.page * settings.perPage) + 1),
-                    cx:settings.cx,
-                    alt:'json'},
-                success:function (data) {
-                    var results = data.items;
-                    if (results) {
-                        resultsDiv.text('');
-                        $(".topicPagination").remove();
-                        // If results were returned, add them to a pageContainer div,
-                        // after which append them to the #resultsDiv:
-                        //var ul = $('<ul>', {className:'documents'});
-                        resultsDiv.append("<ul class='documents'></ul>");
-                        var topicPaginationHtml = '<ul class="topicPagination">' +
-                            '<li class="prev"><a href="#">Previous</a></li>' +
-                            '<li class="next"><a href="#">Next</a></li>' +
-                            '<div class="clear"></div>' +
-                            '</ul><div class="clear"></div>';
-                        $(".documents").append(topicPaginationHtml);
-                        var ul = $(".documents");
-                        for (var i = 0; i < results.length; i++) {
-                            ul.append(new result(results[i]) + '');
-                        }
-                        ul.hide().appendTo(resultsDiv).fadeIn('slow');
-                        resultsDiv.append('<div class="clear"></div>');
-                        $(".documents").append(topicPaginationHtml);
-
-                        if (settings.page > 0) {
-                            $(".prev").click(function () {
-                                googleSearch({append:false, page:settings.page - 1});
-                            });
-                        } else {
-                            $('.prev').bind('click', false);
-                        }
-
-                        if (data.queries.request[0].totalResults > (settings.page + 1) * settings.perPage) {
-                            $(".next").click(function () {
-                                googleSearch({append:false, page:settings.page + 1});
-                            });
-                        } else {
-                            $('.next').bind('click', false);
-                        }
-                    }
-                    else {
-                        // No results were found for this search.
-                        resultsDiv.empty();
-                        $('<p>', {className:'notFound', html:'<h3 class="searchNoResult">Oops, we can\'t find what you were looking for. Try rephrasing your search</h3>'}).hide().appendTo(resultsDiv).fadeIn();
-                    }
-//            $(".breadcrumbs").click(function() {
-//                //$(".breadcrumbs").html('<a href="#">Cloudify Documentation Home</a>');
-//                resultsDiv.text('');
-//                $(".topicPagination").remove();
-//                $("#resultDiv").hide();
-//                $("#pageContent").fadeIn();
-//            });
-                    $("#pageContent").hide(1, function () {
-                        $(".pageTitle").text("Search Results");
-                        $("#resultDiv").fadeIn();
-                        $(".breadcrumb").html('<a href="#">Back to ' + settings.pageTitle + '</a>');
-                    });
-//
-
-                }
-            });
-        }
-        catch (e) {
-            alert(e + '');
-        }
-
-
-    }
-
-    function result(r) {
-
-        // This is class definition. Object of this class are created for
-        // each result. The markup is generated by the .toString() method.
-
-        var arr = [
-            '<li class="reg">',
-            '<div class="raw"><div class="span1"><i class="fa fa-file-text blue size70"></i></div><div class="span8"><a class="docTitle" href="',
-            r.link.match(".*/guide/")?r.link.replace(".html", ""):r.link,
-            '">'
-            , r.title,
-            '</a>',
-            '<br class="clear"/>',
-            r.snippet,'&nbsp;&nbsp;',
-            '<a class="rmore" href="', r.link, '" target="_blank">',
-            '<span>Read More</span></a></div><div class="clear"></div></div></li>'
-        ];
-
-
-        // The toString method.
-        this.toString = function () {
-            return arr.join('');
-        }
-    }
-
-
-});
+//});
