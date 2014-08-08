5b342a02446ea5fd99b8a07e7de1df333a759bdf
689df803f755dec0e382660b5ce4ccd20d11dab7 <uri1803@gmail.com>
2014-08-08 14:08:24 +0300

    Search changes

--- js/search/search.js
+++ js/search/search.js
@@ -1,87 +1,82 @@
-//$(document).ready(function () {
 
-    var autocompleteRenderFunction = function(document_type, item) {
-        var title = item["highlight"]["title"];
-        var section = getSection(item["url"]);
-    
-        if (!title) {
-            title = item["title"]
-        }
-        var sectionText = section.section != "gen"? "[" + section.desc + "] " : ""; 
-        var out = "<p class='title'>" + sectionText + title + "</p>";
-        if (item["highlight"]["sections"]) {
-            return out.concat("<p class='sections'>" + item["highlight"]["sections"] + "</p>");
-        }
-        return out;
-    }
-    
 
-    var resultRenderFunction = function(document_type, item) {        
-        var section = getSection(item["url"]);            
-        var sectionText = section.section != "gen"? "[" + section.desc + "] " : ""; 
-        var out = '<a href="' + item['url'] + '" class="st-search-result-link">' + sectionText + item['title'] + '</a>';
-        return out.concat('<p class="genre">' + item['genre'] + '</p>');
+var autocompleteRenderFunction = function(document_type, item) {
+    var title = item["highlight"]["title"];
+    var section = getSection(item["url"]);
+
+    if (!title) {
+        title = item["title"]
     }
 
-    var Swiftype = window.Swiftype || {};
-    (function() {
-        Swiftype.key = 'MdeBSe5BbjKxTiXEYcxH';
-
-        Swiftype.searchFunctionalBoosts = {
-            "page": {"score": "exponential"}
-        };
-
-        var sections = getSectionsForSearch();
-        Swiftype.searchFilters = function() {
-            return { 'page' : { 'section' : sections}};
-        };
-
-        Swiftype.autocompleteFilters = function() {
-            return { 'page' : { 'section' : sections}};
-        };
+    var sectionText = section.section != "gen"? "[" + section.desc + "] " : ""; 
+    var out = "<p class='title'>" + sectionText + title + "</p>";
+    if (item["highlight"]["sections"]) {
+        return out.concat("<p class='sections'><span class='section'>" + item["highlight"]["sections"] + "</span></p>");
+    }
+    return out;
+}
 
 
-        //  Swiftype.autocompleteRenderFunction = autocompleteRenderFunction; 
-        //Swiftype.autocompleteResultRenderFunction = autocompleteResultRenderFunction;
-        
 
-        /** DO NOT EDIT BELOW THIS LINE **/
-        var script = document.createElement('script'); script.type = 'text/javascript'; script.async = true;
-        script.src = "//s.swiftypecdn.com/embed.js";
-        var entry = document.getElementsByTagName('script')[0];
-        entry.parentNode.insertBefore(script, entry);
-    }());
+var Swiftype = window.Swiftype || {};
+(function() {
+    Swiftype.key = 'MdeBSe5BbjKxTiXEYcxH';
+
+    Swiftype.searchFunctionalBoosts = {
+        "page": {"score": "exponential"}
+    };
+
+    var sections = getSectionsForSearch();
+    Swiftype.searchFilters = function() {
+        return { 'page' : { 'section' : sections}};
+    };
+
+    Swiftype.autocompleteFilters = function() {
+        return { 'page' : { 'section' : sections}};
+    };
+
+
+    Swiftype.autocompleteRenderFunction = autocompleteRenderFunction; 
+    //Swiftype.autocompleteResultRenderFunction = autocompleteResultRenderFunction;
     
 
-    function endsWith(str, suffix) {
-        return str.indexOf(suffix, str.length - suffix.length) !== -1;
+    /** DO NOT EDIT BELOW THIS LINE **/
+    var script = document.createElement('script'); script.type = 'text/javascript'; script.async = true;
+    script.src = "//s.swiftypecdn.com/embed.js";
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(script, entry);
+}());
+
+
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+function startsWith(str, prefix) {
+    return str.indexOf(prefix) == 0;
+}
+
+function getSectionsForSearch() {
+    var section = getSection(window.location.href).section;
+    if (startsWith(section, "guide")) return section; 
+    return ["guide3.0", "gen", "blog"];
+}
+
+function getSection(url) {
+    if (url) {
+        var urlSections = url.split("/");
+        if (urlSections.length > 3) {
+            if (urlSections[3] == "guide") {
+                var version = urlSections.length > 4? urlSections[4]:null; 
+                return {desc: version + " Docs", section:"guide" + version};
+            } else if (urlSections[3] == "blog" || urlSections[3] == "tags" || $.isNumeric(urlSections[3])) {
+                return {desc: "Blog", section:"blog"};
+            } 
+        }             
     }
-
-    function startsWith(str, prefix) {
-        return str.indexOf(prefix) == 0;
-    }
-
-    function getSectionsForSearch() {
-        var section = getSection(window.location.href).section;
-        if (startsWith(section, "guide")) return section; 
-        return ["guide3.0", "gen", "blog"];
-    }
-
-    function getSection(url) {
-        if (url) {
-            var urlSections = url.split("/");
-            if (urlSections.length > 3) {
-                if (urlSections[3] == "guide") {
-                    var version = urlSections.length > 4? urlSections[4]:null; 
-                    return {desc: version + " Docs", section:"guide" + version};
-                } else if (urlSections[3] == "blog" || urlSections[3] == "tags" || $.isNumeric(urlSections[3])) {
-                    return {desc: "Blog", section:"blog"};
-                } 
-            }             
-        }
-        return {desc: "", section:"gen"};            
-    }
-    
+    return {desc: "", section:"gen"};            
+}
 
 
-//});
+
+
