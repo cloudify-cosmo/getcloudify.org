96605751b64cad10175a55569bba04a2859414cc
shar1z <sharoner@gigaspaces.com>
2015-12-29 17:51:17 +0200

    JS file

--- source/js/custom.js
+++ source/js/custom.js
@@ -292,11 +292,11 @@ $(document).ready(function() {
 
 		
 	//Navi hover
-	$('ul.nav li.dropdown').hover(function () {
+	/*$('ul.nav li.dropdown').hover(function () {
 		$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
 	}, function () {
 		$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
-	});
+	});*/
 	
 	// tooltip
 	$('.social-network li a, .options_box .color a').tooltip();
